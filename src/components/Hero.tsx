950a1e34e453b8fdaab5de26e64ea25dead166c8
badkamyk <slothmateusz@gmail.com>
2022-12-01 11:19:30 +0100

    refactored nav component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,6 @@
-/* This example requires Tailwind CSS v3.0+ */
-import {useState} from 'react'
-import {Dialog} from '@headlessui/react'
-import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline'
-
-const navigation = [
-    {name: 'Home', href: '/'},
-    {name: 'Product', href: '/product'},
-    {name: 'Pricing', href: '/pricing'},
-    {name: 'About', href: '/about'},
-    {name: 'Contact', href: '/contact'},
-]
+import Nav from './Nav'
 
 export default function Hero() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-
     return (
         <div className="isolate bg-white">
             <div
@@ -44,96 +31,7 @@ export default function Hero() {
                     </defs>
                 </svg>
             </div>
-            <div className="px-6 pt-6 lg:px-8">
-                <div>
-                    <nav className="flex h-9 items-center justify-between" aria-label="Global">
-                        <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
-                            <a href="#" className="-m-1.5 p-1.5">
-                                <span className="sr-only">Your Company</span>
-                                <img className="h-8"
-                                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
-                            </a>
-                        </div>
-                        <div className="flex lg:hidden">
-                            <button
-                                type="button"
-                                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                                onClick={() => setMobileMenuOpen(true)}
-                            >
-                                <span className="sr-only">Open main menu</span>
-                                <Bars3Icon className="h-6 w-6" aria-hidden="true"/>
-                            </button>
-                        </div>
-                        <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
-                            {navigation.map((item) => (
-                                <a key={item.name} href={item.href}
-                                   className="font-semibold text-gray-900 hover:text-gray-900">
-                                    {item.name}
-                                </a>
-                            ))}
-                        </div>
-                        <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
-                            <a
-                                href="#"
-                                className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                            >
-                                Log in
-                            </a>
-                        </div>
-                    </nav>
-                    <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                        // @ts-ignore
-                        {/*<Dialog.Panel focus="true" className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">*/}
-                        <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
-                            <div className="flex h-9 items-center justify-between">
-                                <div className="flex">
-                                    <a href="#" className="-m-1.5 p-1.5">
-                                        <span className="sr-only">Your Company</span>
-                                        <img
-                                            className="h-8"
-                                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                                            alt=""
-                                        />
-                                    </a>
-                                </div>
-                                <div className="flex">
-                                    <button
-                                        type="button"
-                                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                                        onClick={() => setMobileMenuOpen(false)}
-                                    >
-                                        <span className="sr-only">Close menu</span>
-                                        <XMarkIcon className="h-6 w-6" aria-hidden="true"/>
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="mt-6 flow-root">
-                                <div className="-my-6 divide-y divide-gray-500/10">
-                                    <div className="space-y-2 py-6">
-                                        {navigation.map((item) => (
-                                            <a
-                                                key={item.name}
-                                                href={item.href}
-                                                className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
-                                            >
-                                                {item.name}
-                                            </a>
-                                        ))}
-                                    </div>
-                                    <div className="py-6">
-                                        <a
-                                            href="#"
-                                            className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
-                                        >
-                                            Log in
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </Dialog.Panel>
-                    </Dialog>
-                </div>
-            </div>
+            <Nav />
             <main>
                 <div className="relative px-6 lg:px-8">
                     <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
