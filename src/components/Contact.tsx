26afda473d2a0b23a236d228418695fd5ab7ee9d
badkamyk <slothmateusz@gmail.com>
2022-12-02 13:57:05 +0100

    feature: fetching air quality data

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,9 @@
-import {useForm} from "react-hook-form";
-import {useRef} from "react";
+import { useForm } from "react-hook-form";
+import { useRef } from "react";
 import emailjs from '@emailjs/browser';
 
 export default function Contact() {
-    const {register, handleSubmit, watch, formState: {errors}, resetField, reset} = useForm(
+    const { register, handleSubmit, watch, formState: { errors }, resetField, reset } = useForm(
         {
             defaultValues: {
                 email: "",
@@ -35,32 +35,32 @@ export default function Contact() {
                 <form ref={formRef} className="space-y-8" onSubmit={handleSubmit(handleFormSubmit)}>
                     <div>
                         <label htmlFor="email"
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your
                             email</label>
-                        <input type="email" id="email" {...register("email", {required: true})} autoComplete="email"
-                               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                               placeholder="name@email.com"/>
+                        <input type="email" id="email" {...register("email", { required: true })} autoComplete="email"
+                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                            placeholder="name@email.com" />
                     </div>
                     <div>
                         <label htmlFor="subject"
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                        <input type="text" id="subject" {...register("subject", {required: true})}
-                               autoComplete="subject"
-                               className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                               placeholder="Let us know how we can help you"/>
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+                        <input type="text" id="subject" {...register("subject", { required: true })}
+                            autoComplete="subject"
+                            className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                            placeholder="Let us know how we can help you" />
 
                     </div>
                     <div className="sm:col-span-2">
                         <label htmlFor="message"
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
                             message</label>
-                        <textarea id="message" rows={6} {...register("message", {required: true})}
-                                  autoComplete="message"
-                                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                  placeholder="Leave a message..."></textarea>
+                        <textarea id="message" rows={6} {...register("message", { required: true })}
+                            autoComplete="message"
+                            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Leave a message..."></textarea>
                     </div>
                     <button type="submit"
-                            className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg sm:w-fit bg-indigo-600 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send
+                        className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg sm:w-fit bg-indigo-600 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send
                         message
                     </button>
                 </form>
